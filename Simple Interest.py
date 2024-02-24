23fb5120f78526f3c26cc71d7b25f2c943549b20
Sukhjit <sukhaji65@gmail.com>
2024-02-24 14:04:35 +0530

    SI

---
+++ Simple Interest.py
@@ -0,0 +1,6 @@
+princ = float(input("Enter the Principal Amount:"))
+rate = float(input("Enter the rate of Interest:"))
+time = float(input("Enter the time Period:"))
+si = (princ*rate*time)/100
+print("The simple interest=",si)
+                   
